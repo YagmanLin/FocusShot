a3d7112bcfad0582aa43ff1e90d0e845e6a476cb
Yiming LIn <lin5291@qq.com>
2026-04-05 21:55:37 +0800

    Initial Commit

---
+++ FocusShot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusShot
+//
+//  Created by 王小琛 on 2026/4/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusShot/FocusShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusShotApp.swift
+//  FocusShot
+//
+//  Created by 王小琛 on 2026/4/5.
+//
+
+import SwiftUI
+
+@main
+struct FocusShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
